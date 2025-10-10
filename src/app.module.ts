80d023d356af577dd371982bf4212b6d288f7411
Arthur Axiotes <axiotesarthur@gmail.com>
2025-10-09 23:40:06 -0300

    feat: criacao dos modulos, services e controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmployeeModule } from './modules/employee/employee.module';
+import { ServicesModule } from './common/services/services.module';
 
 @Module({
-  imports: [],
+  imports: [EmployeeModule, ServicesModule],
   controllers: [AppController],
   providers: [AppService],
 })
